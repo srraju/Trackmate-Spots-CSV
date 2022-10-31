2e32456f2e0c14c6e736312e03528f35b5e28cb7
srraju <sr.raju2003@gmail.com>
2022-10-31 12:31:40 +1100

    Update Read_ROI_SpotsCSV.py
    Change the boolean status of csvimportbool to turn on or off trackmate CSV import. The code then just used the active roi to calculate center of mass.

--- Read_ROI_SpotsCSV.py
+++ Read_ROI_SpotsCSV.py
@@ -1,3 +1,4 @@
+from ij import IJ
 import csv
 from ij import WindowManager
 from ij.plugin.frame import RoiManager
@@ -12,52 +13,24 @@ Tracking is done using trackmate
 The sports are exported to a csv file
 Selected ROI is then exported to IJ roi manager in the actions page
 
+CLICK ON THE IMAGE BEFORE RUNNING THE CODE
 '''
 
-csvfilename = '3900.csv'
 
-imp = WindowManager.getCurrentImage()
-    # Add points to ROI manager
-#print(imp.getStack())
+#	method for trackmates spots csv file
+csvimportbool=False
+csvfilename = 'C:/Raju/Trackmate/3900.csv'
 
-'''
-Reading the trackmate output file in csv format
-'''
 
-data=[] #Contains the spot information in the form of one spot per dictionary
-with open(csvfilename) as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		data.append(row) # Each row corresponds to the quantified information of one spot
+def TrackmateCSVimport(csvfilename):
+	data=[] #Contains the spot information in the form of one spot per dictionary
+	with open(csvfilename) as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			data.append(row) # Each row corresponds to the quantified information of one spot
+	return data
 
-'''
-	The spots are stored as a list of dictionary in variable data
-	data[index]['LABEL']) returns the spot id 
-	The spot id is then searched for in the exported ROIs
-	Everytime a match is found, the data is added to the results table
-'''
-
-# Opens / selects the roi manager
-rm = RoiManager.getInstance()
-if not rm:
-    rm = RoiManager()
-
-options = IS.ALL_STATS 
-# calculates all available statistical information for spots in ROI
-count=rm.getCount()
-# All roi's are stored as an array in rois
-rois=rm.getRoisAsArray()
-# An instance to invoke the imagej Results table
-rt=ResultsTable()
-
-for roi in rois:
-	imp.setRoi(roi)
-	stats=imp.getStatistics(options)
-	rt.incrementCounter() # Adds a row in Results table
-	#rt.addValue adds a value corresponding to column name
-	rt.addValue("Slice",roi.getTPosition()) 
-	rt.addValue("Label",roi.getName())
-	spot=roi.getName()
+def getSpotTrackmateData(spot,data):	
 	# This looks for a match with the spot name in the csv file and roi manager
 	# The corresonding index is then used to copy information to results table
 	spotindex = next((index for (index, d) in enumerate(data) if d["LABEL"] == spot), None)
@@ -78,18 +51,72 @@ for roi in rois:
 	    rt.addValue("ELLIPSE ASPECT RATIO",float(data[spotindex]['ELLIPSE_ASPECTRATIO']))
 	    rt.addValue("AREA",float(data[spotindex]['AREA']))
 	    rt.addValue("PERIMETER",float(data[spotindex]['PERIMETER']))
-	    print(roi.getTPosition(),roi.getName())
-	
+	    #print(roi.getTPosition(),roi.getName())
+
+		
+'''
+	The spots are stored as a list of dictionary in variable data
+	data[index]['LABEL']) returns the spot id 
+	The spot id is then searched for in the exported ROIs
+	Everytime a match is found, the data is added to the results table
+'''
+# Assigns the currently opened image to imp
+imp = WindowManager.getCurrentImage()
+
+if csvimportbool:
+	print "importing trackmate csv data"
+	data=TrackmateCSVimport(csvfilename)
+else:
+	print "No trackmate CSV file"
+
+# Opens / selects the roi manager
+rm = RoiManager.getInstance()
+if not rm:
+    rm = RoiManager()
+# calculates all available statistical information for spots in ROI    
+options = IS.ALL_STATS 
+count=rm.getCount()
+# All roi's are stored as an array in rois
+rois=rm.getRoisAsArray()
+# An instance to invoke the imagej Results table
+rt=ResultsTable()
+
+for roi in rois:
+	imp.setRoi(roi)
+	name=roi.getName()
+# If the csv import bool is True, then Z and T are interchanged during trackmate	
+	if csvimportbool:
+		Slice=roi.getTPosition()
+	else:
+		Slice=roi.getTPosition()
+	IJ.setSlice(Slice)
+	rt.incrementCounter() # Adds a row in Results table
+	#rt.addValue adds a value corresponding to column name
+	rt.addValue("Slice",Slice) 
+	rt.addValue("Label",roi.getName())
+	spot=roi.getName()
+# If the csv import bool is True, then getSpotTrtackmateData method retrieves the spot information
+	if csvimportbool:
+		getSpotTrackmateData(spot,data)
+
+	stats=imp.getStatistics(options)
 	rt.addValue("xCOM",stats.xCenterOfMass)
 	rt.addValue("yCOM",stats.yCenterOfMass)
 	rt.addValue("xCentroid",stats.xCentroid)
 	rt.addValue("yCentroid",stats.yCentroid)
-	#rt.addValue("Area",stats.area)
-	#rt.addValue("Mean",stats.mean)
-	#rt.addValue("Max",stats.max)
-	#rt.addValue("Min",stats.min)
-	#rt.addValue("xminor",stats.minor)
-	#rt.addValue("xmajor",stats.major)
+'''
+# To obtain the pixels within the roi	
+	length= len(roi.getContainedPoints())
+	xs=[]
+	ys=[]
+	value=[]
+	for i in range(length):	
+		xs.append(roi.getContainedPoints()[i].x)
+		ys.append(roi.getContainedPoints()[i].y)
+		value.append(imp.getProcessor().getPixelValue(xs[i],ys[i]) )
+
+	print zip(xs,ys,value)		
+'''
 
 rt.sort("Slice")
 length=rt.size()
@@ -97,51 +124,3 @@ print(r'Total number of frames in the roi are ',length)
 rt.show("Spots on a given track as a function of frame number")
 #rt.saveAs("C:\Raju\Trackmate\Single_Track_X_Y_COM_Centroid.csv")
 
-
-'''
-sliceno=rt.getColumn("Slice")
-xcom=rt.getColumn("xCOM")
-ycom=rt.getColumn("yCOM")
-xcentroid=rt.getColumn("xCentroid")
-ycentroid=rt.getColumn("yCentroid")
-
-
-print(sliceno)
-print(xcom)
-print(ycom)
-print(xcentroid)
-print(ycentroid)
-
-# plotting
-
-from ij.gui import Plot
-
-
-# title, X label, Y label
-plot1 = Plot("xCOM_Centroid", "Framee_no", "X")
-
-# Series 1
-plot1.setColor("blue")
-plot1.add("line", sliceno,xcom)
-
-# Series 2
-plot1.setColor("black")
-plot1.add("line",sliceno,xcentroid)
-
-plot1.addLegend("Center of Mass \t Centroid")
-
-
-plot2 = Plot("yCOM_Centroid", "Framee_no", "Y")
-
-# Series 1
-plot2.setColor("blue")
-plot2.add("line", sliceno,ycom)
-
-# Series 2
-plot2.setColor("black")
-plot2.add("line",sliceno,ycentroid)
-plot2.addLegend("Center of Mass \t Centroid")
-
-plot1.show()
-plot2.show()
-'''
